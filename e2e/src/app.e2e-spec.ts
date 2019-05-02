cbfb05d5f2df1d42356babb9f3ba37dbecc9f02d
Amber Gupta <33590295+agupta1897@users.noreply.github.com>
2019-05-02 05:50:43 -0500

    Configuration File Changes

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to drowsyDriver!');
+    expect(page.getTitleText()).toEqual('Welcome to emoteControl!');
   });
 
   afterEach(async () => {
